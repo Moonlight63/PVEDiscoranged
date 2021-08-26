5e3aca46fa6b8a8af883bc3983d06cdc04d338ec
Dalen <dalencattmlsp@gmail.com>
2021-08-26 01:57:35 -0700

    Discoranged

--- PVEDiscordDark/js/PVEDiscordDark.js
+++ PVEDiscordDark/js/PVEDiscordDark.js
@@ -23,7 +23,7 @@ const patchCharts = function() {
 	Ext.chart.theme.Base.prototype.config.chart.defaults.background = '#23272a';
 	Ext.chart.theme.Base.prototype.config.axis.defaults.label.color = 'white';
 	Ext.chart.theme.Base.prototype.config.axis.defaults.title.color = 'white';
-	Ext.chart.theme.Base.prototype.config.axis.defaults.style.strokeStyle = '#7289DA';
+	Ext.chart.theme.Base.prototype.config.axis.defaults.style.strokeStyle = '#e57000';
 	Ext.chart.theme.Base.prototype.config.axis.defaults.grid.strokeStyle = 'rgba(44, 47, 51, 1)';
 	Ext.chart.theme.Base.prototype.config.sprites.text.color = 'white';
 };
@@ -32,7 +32,7 @@ function patchGaugeWidget() {
 	Proxmox.panel.GaugeWidget.prototype.backgroundColor = '#2C2F33';
 	Proxmox.panel.GaugeWidget.prototype.criticalColor = '#f04747';
 	Proxmox.panel.GaugeWidget.prototype.warningColor = '#faa61a';
-	Proxmox.panel.GaugeWidget.prototype.defaultColor = '#7289DA';
+	Proxmox.panel.GaugeWidget.prototype.defaultColor = '#e57000';
 	Proxmox.panel.GaugeWidget.prototype.items[1].series[0].colors[0] = '#2C2F33';
 };
 
